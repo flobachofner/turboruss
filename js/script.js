73cbbee26129454712cf83e4d43976cd8e0420dd
Bambus <florianbachofner@hotmail.com>
2021-10-23 17:03:26 +0200

    commit flo het päde gseit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // JavaScript Document
 
-
-// Add Map
+///////////////////
+// TURBORUSS Map //
+///////////////////
 function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     mapId: "923884f072e6e724",
     center: { lat: 47.3849725, lng: 8.5353119 },
     zoom: 12,
@@ -11,21 +12,81 @@ function initMap() {
 	mapTypeControl: false,
   });
 	
-  new google.maps.Marker({
-	  position: { lat: 47.3849725, lng: 8.5353119 },
-	  map,
-	  title: "Coming soon!",
-	  icon: {
-		  url: "img/favicon.svg",
-		  scaledSize: new google.maps.Size(40, 40)
-	  },
-	  animation: google.maps.Animation.DROP
-  });
+  const iconBase =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
 	
+  const icons = {
+    production: {
+      icon: iconBase + "parking_lot_maps.png",
+    },
+    seller: {
+      icon: iconBase + "library_maps.png",
+    },
+    distributor: {
+      icon: iconBase + "info-i_maps.png",
+    },
+  };
+	
+  const features = [
+    {
+      position: new google.maps.LatLng(47.417494, 8.5573861),
+      type: "production",
+	  name: "Brauerei Oerlikon"
+    },
+	{
+      position: new google.maps.LatLng(47.4116522, 8.5412326),
+      type: "seller",
+	  name: "Drinks of the World Oerlikon"
+    },
+    {
+      position: new google.maps.LatLng(47.3697419, 8.5423319),
+      type: "distributor",
+	  name: "Karl der Grosse"
+    },
+	{
+      position: new google.maps.LatLng(47.3791612, 8.5423464),
+      type: "distributor",
+	  name: "Kleine Freiheit"
+    },
+	{
+      position: new google.maps.LatLng(47.3762496, 8.5447811),
+      type: "distributor",
+	  name: "bQm"
+    },
+	{
+      position: new google.maps.LatLng(47.4981776, 8.7292063),
+      type: "distributor",
+	  name: "Zum hinteren Hecht"
+    },
+  ];
+
+  // Create markers
+  for (let i = 0; i < features.length; i++) {
+	  
+    let infowindow = new google.maps.InfoWindow({
+      content: features[i].name,
+    });
+	
+    let marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+	
+	marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: true,
+      });
+    });
+  }
 }
 
 
-// Scroll smoothly
+/////////////////////
+// Scroll smoothly //
+/////////////////////
 $(function() {
 	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
 		var target = $(this.hash);
@@ -39,6 +100,11 @@ $(function() {
 	});
 });
 
+
+
+//////////////////
+// Modal Window //
+//////////////////
 
 // Get the modal
 var modal = document.getElementById("myModal");
